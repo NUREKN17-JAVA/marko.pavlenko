d324c68fb77d03a92d13d0f9d3c386bbd9e1483f
NUREKN17-JAVA <marko.pavlenko@nure.ua>
2019-11-05 16:43:02 +0200

    We have successfully done the step #15: Получение настроек из внешнего файла (класс DaoFactory)

---
+++ src/main/java/com/markopavlenko/usermanagement/db/DaoFactory.java
@@ -0,0 +1,18 @@
+package com.markopavlenko.usermanagement.db;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class DaoFactory {
+			
+	    private final Properties properties;
+
+	    private DaoFactory() {
+	        properties = new Properties();
+	        try {
+	            properties.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
+	        } catch (IOException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	}
