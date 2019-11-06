be67197a32e4ec06472debb8081cf3b2293a7507
NUREKN17-JAVA <marko.pavlenko@nure.ua>
2019-11-07 00:15:44 +0200

    New commit

--- src/main/java/com/markopavlenko/usermanagement/User.java
+++ src/main/java/com/markopavlenko/usermanagement/User.java
@@ -5,72 +5,69 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class User implements Serializable {
-    private static final long serialVersionUID = 7485L;
+
+    private static final long serialVersionUID = -3760492779402022862L;
     private Long id;
     private String firstName;
     private String lastName;
+    private Date dateOfBirth;
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
-    private Date dateOfBirth;
-
     public String getFullName() {
-        return getFirstName() + " " + getLastName();
+        return new StringBuilder( getLastName())
+            .append(", ")
+            .append(getFirstName())
+            .toString();
     }
 
-    public long getAge() { 
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-    
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        int currentDay = calendar.get(Calendar.DATE);
-        
-        calendar.setTime(getDateOfBirth());
-        
-        int birthYear = calendar.get(Calendar.YEAR);
-        int birthMonth = calendar.get(Calendar.MONTH);
-        int birthDay = calendar.get(Calendar.DATE);
-        
-        int age = currentYear - birthYear;
-        
-        if (currentMonth < birthMonth) {
-            return age--;
-        } else if (currentMonth == birthMonth) {
-            if (currentDay < birthDay) {
-                return age--;
-            }
+    public int getAge() {
+        Calendar calendarCurrent = Calendar.getInstance();
+        calendarCurrent.setTime(new Date());
+
+        Calendar calendarBirth = Calendar.getInstance();
+        calendarBirth.setTime(getDateOfBirth());
+
+        int age = calendarCurrent.get(Calendar.YEAR) - calendarBirth.get(Calendar.YEAR);
+
+        if ((calendarCurrent.get( Calendar.MONTH) == calendarBirth.get( Calendar.MONTH)) &&
+                (calendarCurrent.get( Calendar.DAY_OF_MONTH) > calendarBirth.get( Calendar.DAY_OF_MONTH))) {
+            age--;
         }
+
+        if(calendarCurrent.get( Calendar.MONTH) > calendarBirth.get( Calendar.MONTH))
+            age--;
+
         return age;
     }
 }
