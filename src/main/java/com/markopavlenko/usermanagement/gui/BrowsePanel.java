e4e31735d73276ec50550ccc2361887713103c18
markopavlenko <marko.pavlenko@nure.ua>
2019-11-29 00:01:24 +0200

    We created the main window and the main control panel

---
+++ src/main/java/com/markopavlenko/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,10 @@
+package com.markopavlenko.usermanagement.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+	
+    public BrowsePanel(MainFrame frame) {
+        
+    }
+}
