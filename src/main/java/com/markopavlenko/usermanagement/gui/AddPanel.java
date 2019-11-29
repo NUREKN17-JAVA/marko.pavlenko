01b496b27de5485327a4b837537afab4e3ad52de
markopavlenko <marko.pavlenko@nure.ua>
2019-11-29 18:05:20 +0200

    We have finished the implementation of the sub-panel for data entry fields.

--- src/main/java/com/markopavlenko/usermanagement/gui/AddPanel.java
+++ src/main/java/com/markopavlenko/usermanagement/gui/AddPanel.java
@@ -1,73 +1,119 @@
 package com.markopavlenko.usermanagement.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JPanel;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AddPanel extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 2371425014873030612L;
 	
-		private static final long serialVersionUID = 2371425014873030612L;
-		
-		protected MainFrame parent;
-		protected JPanel buttonPanel;
-		protected JPanel fieldPanel;
-		protected JButton cancelButton;
-	    protected JButton okButton;
+	protected MainFrame parent;
+	protected JPanel buttonPanel;
+	protected JPanel fieldPanel;
+	protected JButton cancelButton;
+    protected JButton okButton;
+    protected JTextField firstNameField;
+    protected JTextField dateOfBirthField;
+    protected JTextField lastNameField;
+    protected Color bgColor;
 
-	    public AddPanel(MainFrame parent) {
-	        this.parent = parent;
-	        initialize();
-	    }
+    public AddPanel(MainFrame parent) {
+        this.parent = parent;
+        initialize();
+    }
 
-	    protected void initialize() {
-	        this.setLayout(new BorderLayout());
-	        this.add(getFieldPanel(), BorderLayout.NORTH);
-	        this.add(getButtonPanel(), BorderLayout.SOUTH);
-	        
-	    }
-		
-		 protected JPanel getFieldPanel() {
-			 return fieldPanel;
-	    }
-		
-	    protected JPanel getButtonPanel() {
-	        if (buttonPanel == null) {
-	            buttonPanel = new JPanel();
-	            buttonPanel.add(getOkButton(), null);
-	            buttonPanel.add(getCancelButton(), null);
-	        }
-	        return buttonPanel;
-	    }
+    protected void initialize() {
+        this.setName("addPanel"); 
+        this.setLayout(new BorderLayout());
+        this.add(getFieldPanel(), BorderLayout.NORTH);
+        this.add(getButtonPanel(), BorderLayout.SOUTH);
+        
+    }
+	
+	 protected JPanel getFieldPanel() {
+        if (fieldPanel == null) {
+            fieldPanel = new JPanel();
+            fieldPanel.setLayout(new GridLayout(3, 2));
+            addLabeledField(fieldPanel,"Имя", getFirstNameField()); 
+            addLabeledField(fieldPanel,"Фамилия", getLastNameField());
+            addLabeledField(fieldPanel,"Дата рождения", getDateOfBirthField()); 
+        }
+        return fieldPanel;
+    }
+	
+    protected JPanel getButtonPanel() {
+        if (buttonPanel == null) {
+            buttonPanel = new JPanel();
+            buttonPanel.add(getOkButton(), null);
+            buttonPanel.add(getCancelButton(), null);
+        }
+        return buttonPanel;
+    }
 
-	    protected JButton getCancelButton() {
-	        if (cancelButton == null) {
-	            cancelButton = new JButton();
-	            cancelButton.setText("Cancel"); 
-	            cancelButton.setName("cancelButton"); 
-	            cancelButton.setActionCommand("cancel"); 
-	            cancelButton.addActionListener(this);
-	        }
-	        return cancelButton;
-	    }
+    protected JButton getCancelButton() {
+        if (cancelButton == null) {
+            cancelButton = new JButton();
+            cancelButton.setText("Отменить"); 
+            cancelButton.setName("cancelButton"); 
+            cancelButton.setActionCommand("cancel"); 
+            cancelButton.addActionListener(this);
+        }
+        return cancelButton;
+    }
 
-	    protected JButton getOkButton() {
-	        if (okButton == null) {
-	            okButton = new JButton();
-	            okButton.setText("OK"); 
-	            okButton.setName("okButton");
-	            okButton.setActionCommand("ok"); 
-	            okButton.addActionListener(this);
-	        }
-	        return okButton;
-	    }
+    protected JButton getOkButton() {
+        if (okButton == null) {
+            okButton = new JButton();
+            okButton.setText("ОК"); 
+            okButton.setName("okButton"); 
+            okButton.setActionCommand("ok"); 
+            okButton.addActionListener(this);
+        }
+        return okButton;
+    }  
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+    protected JTextField getDateOfBirthField() {
+        if (dateOfBirthField == null) {
+            dateOfBirthField = new JTextField();
+            dateOfBirthField.setName("dateOfBirthField"); 
+        }
+        return dateOfBirthField;
+    }
 
+    protected JTextField getLastNameField() {
+        if (lastNameField == null) {
+            lastNameField = new JTextField();
+            lastNameField.setName("lastNameField"); //$NON-NLS-1$
+        }
+        return lastNameField;
+    }
+
+    protected void addLabeledField(JPanel panel, String labelText, JTextField textField) {
+        JLabel label = new JLabel(labelText);
+        label.setLabelFor(textField);
+        panel.add(label);
+        panel.add(textField);
+    }
+
+    protected JTextField getFirstNameField() {
+        if (firstNameField == null) {
+            firstNameField = new JTextField();
+            firstNameField.setName("firstNameField"); //$NON-NLS-1$
+        }
+        return firstNameField;
+    }
+
+    
+    
+    public void actionPerformed(ActionEvent e) {
+    	
+       }
 }
