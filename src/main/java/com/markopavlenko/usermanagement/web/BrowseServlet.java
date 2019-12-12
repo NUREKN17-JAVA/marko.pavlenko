0d17769f6ad45b9e300b2129198d788152291b50
Marko193 <marko.pavlenko@nure.ua>
2019-12-12 17:02:46 +0300

    We began to implement the servlet for getting the user`s list

--- src/main/java/com/markopavlenko/usermanagement/web/BrowseServlet.java
+++ src/main/java/com/markopavlenko/usermanagement/web/BrowseServlet.java
@@ -1,7 +1,64 @@
 package com.markopavlenko.usermanagement.web;
 
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.markopavlenko.usermanagement.User;
+import com.markopavlenko.usermanagement.db.DaoFactory;
+import com.markopavlenko.usermanagement.db.DatabaseException;
+
 
 public class BrowseServlet extends HttpServlet {
+    private static final long serialVersionUID = -8030261012558457487L;
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("add") != null) {
+            add(req, resp);
+        } else if (req.getParameter("edit") != null) {
+            edit(req, resp);
+        } else if (req.getParameter("delete") != null) {
+            delete(req, resp);
+        } else if (req.getParameter("details") != null) {
+            details(req, resp);
+        } else {
+            browse(req, resp);
+        }
+    }
+
+    private void details(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Collection<User> users;
+        try {
+            users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession().setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (DatabaseException e) {
+            throw new ServletException(e);
+        }
+    }
 }
