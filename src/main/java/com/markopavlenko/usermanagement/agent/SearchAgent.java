609cd05daac2a374cf0b05ce9d326f93a12414b7
Marko193 <marko.pavlenko@nure.ua>
2019-12-15 00:41:47 +0300

    We implemented some helper methods for the implementing user records search agent behavior

--- src/main/java/com/markopavlenko/usermanagement/agent/SearchAgent.java
+++ src/main/java/com/markopavlenko/usermanagement/agent/SearchAgent.java
@@ -1,6 +1,12 @@
 package com.markopavlenko.usermanagement.agent;
 
+import java.util.Collection;
+
 import jade.core.Agent;
+import com.markopavlenko.usermanagement.User;
+import com.markopavlenko.usermanagement.agent.exception.SearchException;
+import com.markopavlenko.usermanagement.db.DaoFactory;
+import com.markopavlenko.usermanagement.db.DatabaseException;
 
 public class SearchAgent extends Agent {
     /**
@@ -18,5 +24,21 @@ public class SearchAgent extends Agent {
         super.takeDown();
         System.out.println("Agent " + getAID().getName() + " has finished working!");
     }
+    
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+               
+            }
+        } catch (DatabaseException e) {
+            throw new SearchException(e);
+        }
+    }
 
+    public void showUsers(Collection<User> users) {
+        
+    }
 }
